66b88238e9243c7000b17d890953b0ddac2b7d52
George Toothman <george.toothman@flatironschool.com>
2015-08-18 20:32:07 -0400

    add methods to get and search JSON

--- lib/spotify_chart.rb
+++ lib/spotify_chart.rb
@@ -1,54 +1,38 @@
 require 'json'
 require 'open-uri'
-
 class SpotifyChart
 
-  BASE_URL = "?"
+  BASE_URL = "http://charts.spotify.com/api/tracks/most_streamed"
 
   def get_url(region)
-    # return a string that is the BASE_URL/region/weekly/latest
+    BASE_URL + "/" + region + "/" + "weekly" + "/" + "latest"
   end
 
   def get_json(url)
+    JSON.load(open(url))
     # load json given a url here
     # refer to the references if you have questions about this
   end
 
   def get_first_track_info(music_hash)
-    # example music_hash:
-    # {  
-    #   "tracks" => [  
-    #     {  
-    #       "date"         =>"2014-09-14",
-    #       "track_name"   =>"All About That Bass",
-    #       "artist_name"  =>"Meghan Trainor",
-    #       "album_name"   =>"Title"
-    #     },
-    #     {  
-    #       "date"         =>"2014-09-14",
-    #       "track_name"   =>"Break Free",
-    #       "artist_name"  =>"Ariana Grande",
-    #       "album_name"   =>"Break Free"
-    #     }
-    #   ]
-    # }
-
-    # given a hash, like the one above, this method should return a string like:
-    # <track name> by <artist name> from the <album name>
-  
-    # the track name, artist name, and album name should be the first track in the
-    # tracks array
+    track_name = music_hash["tracks"][0]["track_name"]
+    album_name = music_hash["tracks"][0]["album_name"]
+    artist_name = music_hash["tracks"][0]["artist_name"]
+    track_name + " by " + artist_name + " from the album " + album_name
   end
 
 
   def most_streamed(region)
-    # call on #get_url here, where preference is the string 'most_streamed',
-    # and set it equal to a variable
-
-    # call on #get_json here, using the string that get_url returns
-    
-    # finally, call on #get_first_track_info using the 
-    # hash that #get_json returns
+    final_url = self.get_url(region)
+    get_first_track_info(get_json(final_url))
+    # final_url = region.get_url
+    # # call on #get_url here, where preference is the string 'most_streamed',
+    # # and set it equal to a variable
+    # region.get_json(final_url)
+    # # call on #get_json here, using the string that get_url returns
+    # region.get_first_track_info(final_url)
+    # # finally, call on #get_first_track_info using the 
+    # # hash that #get_json returns
   end
 
 end
